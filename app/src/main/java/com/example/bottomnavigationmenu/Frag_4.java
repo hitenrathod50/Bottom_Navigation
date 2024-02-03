4ed9392a7e6a7b33cbeb9fddd2b8a27cd47eafd9
rathodhiten50 <rathodhiten50@gmail.com>
2024-02-03 16:25:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationmenu/Frag_4.java
@@ -0,0 +1,36 @@
+package com.example.bottomnavigationmenu;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Frag_4 extends Fragment {
+
+    Context context;
+    String s;
+    Button toastName;
+
+
+    public Frag_4(Context c) {
+        context = c;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        s = getArguments().getString("Name");
+        return inflater.inflate(R.layout.fragment_frag_2, container, false);
+    }
+
+    public void toastName(View v){
+        Toast.makeText(context, "Name is "+s, Toast.LENGTH_SHORT).show();
+    }
+}
